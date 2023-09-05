b224c076a99577d40c63434b4383b3429bd7b201
oguuk <ogw135@gmail.com>
2023-09-05 22:51:11 +0900

    Initial Commit

---
+++ UpCoin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UpCoin
+//
+//  Created by oguuk on 2023/09/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
