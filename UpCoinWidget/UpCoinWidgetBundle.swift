1b1b8c11ed1bd3788781eae280b1d8031ce2a1af
oguuk <ogw135@gmail.com>
2023-09-09 23:43:57 +0900

    refactor : UpCoinWidgetBundle 추가

---
+++ UpCoinWidget/UpCoinWidgetBundle.swift
@@ -0,0 +1,17 @@
+//
+//  UpCoinWidgetBundle.swift
+//  UpCoinWidget
+//
+//  Created by oguuk on 2023/09/07.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct UpCoinWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        UpCoinWidget()
+        UpCoinWidgetLiveActivity()
+    }
+}
