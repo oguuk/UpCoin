66df72103418d978522c1ea80ddfc37e3491b4f8
oguuk <ogw135@gmail.com>
2023-11-04 21:23:27 +0900

    feat : 밀리초 계산

--- UpCoin/Util/Double + StringFormatting.swift
+++ UpCoin/Util/Double + StringFormatting.swift
@@ -9,7 +9,35 @@ import Foundation
 
 extension Double {
     
+    var formattedWithSeparator: String {
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .decimal
+        return numberFormatter.string(from: NSNumber(value: self)) ?? ""
+    }
+    
+    func convertToMillionUnit() -> String {
+        return (self / 1000000.0).toString() + "M"
+    }
+    
+    func toString() -> String {
+        return "\(self)"
+    }
+    
     func toPercentage(_ place: Int, _ multiply: Double = 1.0) -> String {
         return String(format: "%.\(place)f", self * multiply)
+    }
+    
+    func miliSecondsToDate() -> (year: Int, month: Int, day: Int, hour: Int, minute: Int, second: Int) {
+        let date = Date(timeIntervalSince1970: TimeInterval(self) / 1000.0) // 밀리초 단위를 초 단위로 바꿈
+
+        let calendar = Calendar.current
+
+        let year = calendar.component(.year, from: date)
+        let month = calendar.component(.month, from: date)
+        let day = calendar.component(.day, from: date)
+        let hour = calendar.component(.hour, from: date)
+        let minute = calendar.component(.minute, from: date)
+        let second = calendar.component(.second, from: date)
+        return (year, month, day, hour, minute, second)
     }
 }
