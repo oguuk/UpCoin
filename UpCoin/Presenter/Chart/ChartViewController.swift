65b3ba6e7ff33090e2231ea018b32e39a0f71351
oguuk <ogw135@gmail.com>
2023-11-04 21:15:30 +0900

    feat : ChartViewController

--- UpCoin/Presenter/Chart/ChartViewController.swift
+++ UpCoin/Presenter/Chart/ChartViewController.swift
@@ -5,4 +5,30 @@
 //  Created by oguuk on 2023/09/14.
 //
 
-import Foundation
+import UIKit
+import DGCharts
+
+final class ChartViewController: UIViewController {
+    
+    private var chartView: UIView
+    var ticker: TickerResponse
+    
+    required init(_ ticker: TickerResponse, _ chartView: UIView) {
+        self.chartView = chartView
+        self.ticker = ticker
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func loadView() {
+        super.loadView()
+        self.view = chartView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
