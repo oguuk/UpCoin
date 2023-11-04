86d9625058093d4c0e9c9f7b9523d1a67e652e0b
oguuk <ogw135@gmail.com>
2023-11-04 21:22:11 +0900

    feat : 일봉차트 단위 Formatter

---
+++ UpCoin/Util/AxisValueFormatter/DayAxisValueFormatter.swift
@@ -0,0 +1,24 @@
+//
+//  DayAxisValueFormatter.swift
+//  UpCoin
+//
+//  Created by oguuk on 2023/09/26.
+//
+
+import DGCharts
+import Foundation
+
+final class DayAxisValueFormatter: NSObject, AxisValueFormatter {
+    let dateFormatter: DateFormatter
+
+    override init() {
+        dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MM/dd"
+        super.init()
+    }
+
+    func stringForValue(_ value: Double, axis: AxisBase?) -> String {
+        let date = Date(timeIntervalSince1970: value)
+        return dateFormatter.string(from: date)
+    }
+}
