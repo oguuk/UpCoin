3310d27f326536bd2597f4122e45101f4118ad54
oguuk <ogw135@gmail.com>
2023-11-04 20:38:04 +0900

    feat : 캔들 RestfulAPI 호출 타입 정의

--- UpCoin/Manager/Protocol/CandleAPIType.swift
+++ UpCoin/Manager/Protocol/CandleAPIType.swift
@@ -7,6 +7,18 @@
 
 import Foundation
 
-enum CandleAPIType {
-    case minute, day, week, month
+enum CandleAPIType: String {
+    case day = "/days"
+    case week = "/weeks"
+    case month = "/months"
+}
+// 1, 3, 5, 15, 10, 30, 60, 240
+enum AvailableMinutesUnit: String {
+    case one = "/1"
+    case three = "/3"
+    case five = "/5"
+    case fifteen = "/15"
+    case thirty = "/30"
+    case sixty = "/60"
+    case twoHundredFourty = "/240"
 }
